f669ad658d4c6bedef82668be48094d3249462ce
Jiajun Lu <99420474+LGG1997@users.noreply.github.com>
2022-07-09 03:14:59 +0800

    Update edge_sum.v

--- edge_sum.v
+++ edge_sum.v
@@ -20,106 +20,90 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module edge_sum
-	#(
-		parameter INDEX = 0
-	)(    
-		input clk,
-		input rst,
-		input [47:0] din,
-    	output reg [15:0] sum_position
-	);
+module edge_sum #(
+	parameter INDEX = 0
+)(    
+	input i_clk,
+	input i_rst,
+	input [47:0] i_din,
+    output reg [15:0] o_sum_position
+);
 
-
-wire [4:0] sum_position_tmp[7:0];
-wire [2:0] num_edge_tmp[7:0];
-reg [7:0] num_edge_tmp_1[7:0];
-reg [9:0] tmp_edg_1;
-reg [9:0] tmp_edg_2;
-reg [9:0] tmp_edg_buff;
-reg [5:0] tmp_num_edg;
-reg [5:0] tmp_num_edg_1;
-reg [5:0] tmp_num_edg_buff1;
-reg [5:0] tmp_num_edg_buff2;
-reg [7:0] tmp_pos;
-reg [7:0] tmp_pos_1;
-reg [7:0] tmp_pos_buff1;
-reg [7:0] tmp_pos_buff2;
-reg [15:0] sum_position_tmp1;
-reg [15:0] sum_position_tmp2;
+wire [4:0]  sum_position[7:0];
+wire [2:0]  num_edge[7:0];
+//-----------------------------------------
+reg [5:0]   tmp_num_edge_ff1;
+reg [5:0]   tmp_num_edge_ff2;
+reg [5:0]   tmp_num_edge_ff3;
+reg [5:0]   tmp_num_edge_ff4;
+reg [7:0]   tmp_sum_pos_ff1;
+reg [7:0]   tmp_sum_pos_ff2;
+reg [7:0]   tmp_sum_pos_ff3;
+reg [7:0]   tmp_sum_pos_ff4;
+reg [15:0]  tmp_num_edge_offset[7:0];
+reg [15:0]  tmp_num_edge_offset_ff1;
+reg [15:0]  tmp_num_edge_offset_ff2;
+reg [15:0]  tmp_num_edge_offset_ff3;
+reg [15:0]  sum_position_ff1;
+reg [15:0]  sum_position_ff2;
+//-----------------------------------------
 
 genvar i;
 generate 
-	for(i = 0; i < 8; i = i + 1) begin	
-			sub_edge_sum ses_inst(
-				.clk(clk),
-				.rst(rst),
-				.din(din[(i+1)*6-1:i*6]),
-				.sum_position_tmp(sum_position_tmp[i]),
-				.num_edge_tmp(num_edge_tmp[i])
+	for(i = 0; i < 8; i = i + 1) begin	: sub_edge_sum_block
+			sub_edge_sum U_SUB_EDGE_SUM_INST(
+				.i_clk(i_clk),
+				.i_rst(i_rst),
+				.i_din(i_din[(i+1)*6-1:i*6]),
+				.o_sum_position(sum_position[i]),
+				.o_num_edge(num_edge[i])
 			);
 	end
 endgenerate
 
-
 // Calculating the number of edges in this block
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_num_edg <= 0;
-	else
-		tmp_num_edg <= (num_edge_tmp[0] + num_edge_tmp[1]) + (num_edge_tmp[2]) + (num_edge_tmp[3]);
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_num_edge_ff1 <= 0;
+	    tmp_num_edge_ff2 <= 0;
+	end
+	else begin
+		tmp_num_edge_ff1 <= (num_edge[0] + num_edge[1]) + (num_edge[2]) + (num_edge[3]);
+		tmp_num_edge_ff2 <= (num_edge[4] + num_edge[5]) + (num_edge[6]) + (num_edge[7]);
+    end
 end 
-
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_num_edg_1 <= 0;
-	else
-		tmp_num_edg_1 <= (num_edge_tmp[4] + num_edge_tmp[5]) + (num_edge_tmp[6]) + (num_edge_tmp[7]);
-end 
-
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_num_edg_buff1 <= 0;
-	else
-		tmp_num_edg_buff1 <= tmp_num_edg + tmp_num_edg_1;
-end 
-
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_num_edg_buff2 <= 0;
-	else
-		tmp_num_edg_buff2 <= tmp_num_edg_buff1;
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_num_edge_ff3 <= 0;
+		tmp_num_edge_ff4 <= 0;
+	end
+	else begin
+		tmp_num_edge_ff3 <= tmp_num_edge_ff1 + tmp_num_edge_ff2;
+		tmp_num_edge_ff4 <= tmp_num_edge_ff3;
+    end
 end 
 
 // Calculating the sum of the position of edges
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_pos <= 0;
-	else
-		tmp_pos <= ((sum_position_tmp[0] + sum_position_tmp[1]) +  (sum_position_tmp[2] + sum_position_tmp[3]));
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_sum_pos_ff1 <= 0;
+		tmp_sum_pos_ff2 <= 0;
+	end 
+	else begin
+		tmp_sum_pos_ff1 <= ((sum_position[0] + sum_position[1]) +  (sum_position[2] + sum_position[3]));
+		tmp_sum_pos_ff2 <= ((sum_position[4] + sum_position[5]) + (sum_position[6] +  sum_position[7]));
+	end
 end 
-
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_pos_1 <= 0;
-	else
-		tmp_pos_1 <= ((sum_position_tmp[4] +  sum_position_tmp[5]) + (sum_position_tmp[6] +  sum_position_tmp[7]));
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_sum_pos_ff3 <= 0;
+		tmp_sum_pos_ff4 <= 0;
+	end
+	else begin
+		tmp_sum_pos_ff3 <= tmp_sum_pos_ff1 + tmp_sum_pos_ff2;
+		tmp_sum_pos_ff4 <= tmp_sum_pos_ff3;
+	end
 end 
- 
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_pos_buff1 <= 0;
-	else
-		tmp_pos_buff1 <= tmp_pos + tmp_pos_1;
-end 
-
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_pos_buff2 <= 0;
-	else
-		tmp_pos_buff2 <= tmp_pos_buff1;
-end 
-
 
 /* 
 The purpose of the part is to calculate the offset index used for cascading encoders, and the component requires 2-level pipelines to align "tmp_pos_2" and "tmp_num_edg_2"
@@ -129,73 +113,66 @@ while the correct answer shoule be 286 (47 + 48 + 95 + 96). Note: The formula ca
 caught in second block,and "1" is the block offset index. (The first block index is 0, second one is 1, third one is 2 ...).
 Hence, the modified encoder should output (the sum of the edge position) + (the number of the edge * 48 * offset indexs).
 */
-
-always @(posedge clk or posedge rst) begin
-	if(rst) begin
-		num_edge_tmp_1[0] <= 0;
-		num_edge_tmp_1[1] <= 0;
-		num_edge_tmp_1[2] <= 0;
-		num_edge_tmp_1[3] <= 0;
-		num_edge_tmp_1[4] <= 0;
-		num_edge_tmp_1[5] <= 0;
-		num_edge_tmp_1[6] <= 0;
-		num_edge_tmp_1[7] <= 0;
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_num_edge_offset[0] <= 0;
+		tmp_num_edge_offset[1] <= 0;
+		tmp_num_edge_offset[2] <= 0;
+		tmp_num_edge_offset[3] <= 0;
+		tmp_num_edge_offset[4] <= 0;
+		tmp_num_edge_offset[5] <= 0;
+		tmp_num_edge_offset[6] <= 0;
+		tmp_num_edge_offset[7] <= 0;
 	end
 	else begin
-		num_edge_tmp_1[0] <= num_edge_tmp[0]*0;
-		num_edge_tmp_1[1] <= num_edge_tmp[1]*6;
-		num_edge_tmp_1[2] <= num_edge_tmp[2]*12;
-		num_edge_tmp_1[3] <= num_edge_tmp[3]*18;
-		num_edge_tmp_1[4] <= num_edge_tmp[4]*24;
-		num_edge_tmp_1[5] <= num_edge_tmp[5]*30;
-		num_edge_tmp_1[6] <= num_edge_tmp[6]*36;
-		num_edge_tmp_1[7] <= num_edge_tmp[7]*42;
+		tmp_num_edge_offset[0] <= num_edge[0]*0;
+		tmp_num_edge_offset[1] <= num_edge[1]*6;
+		tmp_num_edge_offset[2] <= num_edge[2]*12;
+		tmp_num_edge_offset[3] <= num_edge[3]*18;
+		tmp_num_edge_offset[4] <= num_edge[4]*24;
+		tmp_num_edge_offset[5] <= num_edge[5]*30;
+		tmp_num_edge_offset[6] <= num_edge[6]*36;
+		tmp_num_edge_offset[7] <= num_edge[7]*42;
 	end		  		
 end 
 
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_edg_1 <= 0;
-	else
-		tmp_edg_1 <= (num_edge_tmp_1[0] + num_edge_tmp_1[1]) + (num_edge_tmp_1[2] + num_edge_tmp_1[3]);			  		
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_num_edge_offset_ff1 <= 0;
+		tmp_num_edge_offset_ff2 <= 0;
+    end
+	else begin
+		tmp_num_edge_offset_ff1 <= (tmp_num_edge_offset[0] + tmp_num_edge_offset[1]) + (tmp_num_edge_offset[2] + tmp_num_edge_offset[3]);
+		tmp_num_edge_offset_ff2 <= (tmp_num_edge_offset[4] + tmp_num_edge_offset[5]) + (tmp_num_edge_offset[6] + tmp_num_edge_offset[7]);
+	end			  		
 end 
 
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_edg_2 <= 0;
-	else
-		tmp_edg_2 <= (num_edge_tmp_1[4] + num_edge_tmp_1[5]) + (num_edge_tmp_1[6] + num_edge_tmp_1[7]);			  		
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst) begin
+		tmp_num_edge_offset_ff3 <= 0;
+    end
+	else begin
+		tmp_num_edge_offset_ff3 <= (tmp_num_edge_offset_ff1 + tmp_num_edge_offset_ff2);
+	end			  		
 end 
 
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		tmp_edg_buff <= 0;
-	else
-		tmp_edg_buff <= tmp_edg_1 + tmp_edg_2;			  		
+always @(posedge i_clk or posedge i_rst) begin 
+	if(i_rst) begin
+		sum_position_ff1 <= 0;
+		sum_position_ff2 <= 0;
+    end
+	else begin
+		sum_position_ff1 <= (tmp_sum_pos_ff4 + tmp_num_edge_offset_ff3);
+		sum_position_ff2 <= (tmp_num_edge_ff4*48*INDEX);
+	end
 end 
 
 // Output
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		sum_position_tmp1 <= 0;
+always @(posedge i_clk or posedge i_rst) begin
+	if(i_rst)
+		o_sum_position <= 0;
 	else
-		sum_position_tmp1 <= (tmp_pos_buff2 + tmp_edg_buff);
-end 
-
-// Output
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		sum_position_tmp2 <= 0;
-	else
-		sum_position_tmp2 <= (tmp_num_edg_buff2*48*INDEX);
-end
-
-// Output
-always @(posedge clk or posedge rst) begin
-	if(rst)
-		sum_position <= 0;
-	else
-		sum_position <= sum_position_tmp1 + sum_position_tmp2;
+		o_sum_position <= sum_position_ff1 + sum_position_ff2;
 end 
 
 endmodule
